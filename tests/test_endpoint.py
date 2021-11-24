3272cbec7bc607c7b88ac7e002666b9d613c4511
mya <yatsenkomy@yandex.ru>
2021-11-24 22:28:58 +0300

    Tests todo created

--- tests/test_endpoint.py
+++ tests/test_endpoint.py
@@ -8,3 +8,38 @@ def test_root():
     response = client.get('/')
     assert response.status_code == 200
     assert response.json() == {'Hello': 'World'}
+
+
+# Test 1: User can succesfully login
+def test_login():
+    pass
+
+
+# Test 2: User gets 403 on invalid credentials
+def test_bad_credentials():
+    pass
+
+
+# Test 3: User receives 401 on expired token
+def test_expired_token():
+    pass
+
+
+# Test 4: User can refresh access token using refresh token
+def test_refresh_access_token():
+    pass
+
+
+# Test 5: User can use refresh token only once
+def test_refresh_token_used_once():
+    pass
+
+
+# Test 6: Refresh tokens become invalid on logout
+def test_invalid_refresh_token_on_logout():
+    pass
+
+
+# Test 7: Multiple refresh tokens are valid
+def test_multiple_refresh_tokens():
+    pass
